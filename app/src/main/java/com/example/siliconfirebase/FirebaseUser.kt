9d2ade8dd845bf8621aa29a18b350320868d369a
Smruti535 <138751739+Smruti535@users.noreply.github.com>
2024-06-29 20:17:41 +0530

    firebase userlist

---
+++ app/src/main/java/com/example/siliconfirebase/FirebaseUser.kt
@@ -0,0 +1,9 @@
+package com.example.siliconfirebase
+
+data class FirebaseUser (
+    val name: String = "",
+    val age: Int = 0,
+    val isAdult: Boolean = false,
+    val userId: String = ""
+
+    )
--- app/src/main/java/com/example/siliconfirebase/MainActivity.kt
+++ app/src/main/java/com/example/siliconfirebase/MainActivity.kt
@@ -13,19 +13,25 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
@@ -37,6 +43,7 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.google.firebase.Firebase
 import com.google.firebase.FirebaseException
 import com.google.firebase.FirebaseTooManyRequestsException
 import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
@@ -44,22 +51,31 @@ import com.google.firebase.auth.FirebaseAuthMissingActivityForRecaptchaException
 import com.google.firebase.auth.PhoneAuthCredential
 import com.google.firebase.auth.PhoneAuthOptions
 import com.google.firebase.auth.PhoneAuthProvider
+import com.google.firebase.firestore.firestore
 import java.util.concurrent.TimeUnit
+import android.app.AlertDialog
+import android.content.Context
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Card
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.KeyboardType
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //enableEdgeToEdge()
         setContent {
-            OTPScreen()
+            AppNavigation()
         }
     }
 
 
 
     val auth = FirebaseAuth.getInstance()
+    val firebaseDB = Firebase.firestore
     var storedVerificationId: String? = null
     lateinit var resendToken: PhoneAuthProvider.ForceResendingToken
+
     fun signUP(email: String, password: String) {
         auth.createUserWithEmailAndPassword(email, password)
             .addOnCompleteListener { task ->
@@ -165,12 +181,74 @@ class MainActivity : ComponentActivity() {
             }
     }
 
+    fun addUserToFirebaseDB(name: String, age: Int){
+        val isAdult = age>=18
+        val firebaseUser =FirebaseUser(name, age, isAdult)
+        firebaseDB.collection("users")
+            .add(firebaseUser)
+            .addOnSuccessListener { dRef ->
+                Log.d(TAG, "Document added with ID: ${dRef.id}")
+            }
+            .addOnFailureListener{ e ->
+                Log.w(TAG, "Document could not be added")
+            }
+    }
+
+    fun updateUser(user: FirebaseUser, newUser: FirebaseUser) {
+        firebaseDB.collection("users").document(user.userId)
+            .update(
+                mapOf(
+                    "name" to newUser.name,
+                    "age" to newUser.age,
+                    "adultOrNot" to (newUser.age>=18)
+                )
+            )
+            .addOnSuccessListener {
+                Log.d(TAG, "Successfully updated")
+            }
+            .addOnFailureListener {
+                Log.w(TAG, "Failed to update: ${it.message}")
+            }
+
+    }
+
+    fun deleteUser(user: FirebaseUser){
+        firebaseDB.collection("users").document(user.userId)
+            .delete()
+            .addOnSuccessListener {
+                Log.d(TAG, "Successfully deleted")
+            }
+            .addOnFailureListener {
+                Log.w(TAG, "Unsuccessful delete operation")
+            }
+    }
+
+    fun fetchFirebaseUsers( onResult: (List<FirebaseUser>)->Unit ){
+        firebaseDB.collection("users")
+            .get()
+            .addOnSuccessListener { result ->
+                val usersList = result.map{ document ->
+                    document.toObject(FirebaseUser::class.java)
+                    val user = document.toObject(FirebaseUser::class.java)
+                    user.copy(userId=document.id)
+                }
+                onResult(usersList)
+            }
+            .addOnFailureListener{ e->
+                Log.w(TAG, "Error getting data", e)
+            }
+    }
+
     @Composable
     fun AppNavigation() {
         val navController = rememberNavController()
-        NavHost(navController = navController, startDestination = "signup") {
+        NavHost(navController = navController, startDestination = "addUser") {
             composable("signup") { SignUpScreen(navController) }
             composable("login") { LoginScreen(navController) }
+            composable("otp") { OTPScreen(navController) }
+            composable("addUser") { AddUserScreen(navController) }
+            composable("allUsers") { AllUserScreen(navController) }
+            //composable("updateUser") { UpdateUserScreen(navController) }
         }
     }
 
@@ -257,7 +335,7 @@ class MainActivity : ComponentActivity() {
     }
 
     @Composable
-    fun OTPScreen(){
+    fun OTPScreen(navController: NavController){
         val phoneNumber = remember { mutableStateOf("") }
         val otpCode = remember { mutableStateOf("") }
         Column(
@@ -280,6 +358,7 @@ class MainActivity : ComponentActivity() {
             }) {
                 Text(text = "Send OTP")
             }
+            Spacer(modifier = Modifier.height(16.dp))
             OutlinedTextField(
                 value = otpCode.value, onValueChange = {
                     otpCode.value = it
@@ -296,6 +375,155 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+
+    @Composable
+    fun AddUserScreen(navController: NavController){
+        val name = remember { mutableStateOf("") }
+        val age = remember { mutableStateOf("") }
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(20.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            OutlinedTextField(
+                value = name.value, onValueChange = {
+                    name.value = it
+                },
+                label = { Text(text = "Enter Name") },
+                modifier = Modifier.fillMaxWidth()
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            OutlinedTextField(
+                value = age.value, onValueChange = {
+                    age.value = it
+                },
+                label = { Text(text = "Enter Age") },
+                modifier = Modifier.fillMaxWidth()
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            Button(onClick = {
+               addUserToFirebaseDB(name.value, age.value.toInt())
+                name.value = ""
+                age.value = ""
+            }) {
+                Text(text = "Add User")
+            }
+            Button(onClick = {
+                navController.navigate("allUsers")
+            }) {
+                Text(text = "Show all users")
+            }
+
+        }
+    }
+
+    @Composable
+    fun UpdateUserDialog(
+        user: FirebaseUser, onDismiss: () -> Unit,
+        onUpdate: (String, Int) -> Unit
+    ) {
+        var name by remember { mutableStateOf(user.name) }
+        var age by remember { mutableStateOf(user.age.toString()) }
+
+        AlertDialog(onDismissRequest = onDismiss, confirmButton = {  Button(
+            onClick = {
+                onUpdate(name, age.toIntOrNull() ?: user.age)
+            }
+        ) {
+            Text("Update")
+        }
+        }, text = {
+            Column {
+                OutlinedTextField(
+                    value = name,
+                    onValueChange = { name = it },
+                    label = { Text("Name") }
+                )
+                OutlinedTextField(
+                    value = age,
+                    onValueChange = { age = it },
+                    label = { Text("Age") },
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+                )
+            }
+        })
+    }
+
+    @Composable
+    fun AllUserScreen(navController: NavController){
+        val userList = remember {
+            mutableStateOf<List<FirebaseUser>>(emptyList())
+        }
+        var selectedUser = remember {
+            mutableStateOf<FirebaseUser?>(null)
+        }
+        var showDialog = remember {
+            mutableStateOf(false)
+        }
+        LaunchedEffect(Unit) {
+            fetchFirebaseUsers { users ->
+                userList.value = users
+            }
+        }
+
+        if (showDialog.value && selectedUser.value != null) {
+            UpdateUserDialog(
+                user = selectedUser.value!!,
+                onDismiss = { showDialog.value = false },
+                onUpdate = { name, age ->
+                    updateUser(selectedUser.value!!, FirebaseUser(name, age))
+                    showDialog.value = false
+                    fetchFirebaseUsers {
+                        userList.value=it
+                    }
+                }
+            )
+        }
+        LazyColumn (
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(20.dp)
+        ){
+            items(userList.value){ user ->
+                Card(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(150.dp)
+                        .padding(8.dp)
+                ) {
+                    Row(
+                        modifier = Modifier.padding(8.dp),
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        Text(
+                            text = "Name: ${user.name},\nAge: ${user.age}",
+                            fontSize = 30.sp,
+                            modifier = Modifier.padding(12.dp)
+                        )
+                        Column() {
+                            Button(onClick = {
+                                selectedUser.value = user
+                                showDialog.value = true
+                            }) {
+                                Text(text = "Update")
+                            }
+                            Button(onClick = {
+                                deleteUser(user)
+                                fetchFirebaseUsers {
+                                    userList.value = it
+                                }
+                            }) {
+                                Text(text = "Delete")
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
 
 
 
